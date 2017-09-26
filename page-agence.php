0c0aabb5d9e1389e0bd8719018c357ac2222a123
Pierre Fritsch <fritschpierre@gmail.com>
2017-09-26 09:16:51 +0200

    New page agence

--- page-agence.php
+++ page-agence.php
@@ -56,10 +56,7 @@ endif; ?>
           <?php show_post('sylvain-schreck'); ?>
         </article>
       </div>
-			<div class="team-pictures">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/team-leo.jpg" width="190" height="589" alt="La team" class="team-photo" data-member="leonard" />
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/team-sylvain.jpg" width="190" height="589" alt="La team" class="team-photo" data-member="sylvain"/>
-			</div>
+			<?php /*<div class="team-pictures"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/team-leo.jpg" width="190" height="589" alt="La team" class="team-photo" data-member="leonard" /><img src="<?php echo get_template_directory_uri(); ?>/assets/images/team-sylvain.jpg" width="190" height="589" alt="La team" class="team-photo" data-member="sylvain"/></div> */ ?>
     </div>
   </div>
 </section>
