9a9c702077063fd40963f46c6ce5ad8dbdd94585
Pierre Fritsch <fritschpierre@gmail.com>
2017-09-26 08:20:04 +0200

    New gallery grid

--- template-parts/project-item.php
+++ template-parts/project-item.php
@@ -1,17 +1,21 @@
 <article class="portfolio-item">
-  <a href="<?php the_permalink(); ?>" class="post-link">
+  <div class="post-link">    
     <?php if ( has_post_thumbnail() ) :
       $id = get_post_thumbnail_id();
       $alt = get_post_meta( $id, '_wp_attachment_image_alt', true);
       ?>
       <figure class="post-thumbnail">
-        <img src="<?php echo the_post_thumbnail_url('fp-small'); ?>" data-interchange="[<?php echo the_post_thumbnail_url('fp-medium'); ?>, small], [<?php echo the_post_thumbnail_url('fp-large'); ?>, medium]" alt="<?php echo esc_attr( $alt );?>" />
-        <noscript><img src="<?php echo the_post_thumbnail_url('fp-large'); ?>" alt="<?php echo esc_attr( $alt );?>" /></noscript>
+        <a href="<?php the_permalink(); ?>">
+          <img src="<?php echo the_post_thumbnail_url('fp-small'); ?>" data-interchange="[<?php echo the_post_thumbnail_url('fp-medium'); ?>, small], [<?php echo the_post_thumbnail_url('fp-large'); ?>, medium]" alt="<?php echo esc_attr( $alt );?>" />
+          <noscript><img src="<?php echo the_post_thumbnail_url('fp-large'); ?>" alt="<?php echo esc_attr( $alt );?>" /></noscript>
+        </a>
       </figure>
     <?php endif; ?>
     <div class="post-content">
-      <h2 class="post-title"><?php the_title(); ?></h2>
+      <a href="<?php the_permalink(); ?>">
+        <h2 class="post-title"><?php the_title(); ?></h2>
+      </a>
       <?php the_category(); ?>
     </div>
-  </a>
+</div>
 </article>
--- template-parts/projects-list.php
+++ template-parts/projects-list.php
@@ -1,7 +1,7 @@
 <section id="portfolio" class="portfolio-list">
 	<div class="row">
 		<div class="main-content">
-			<?php $loop = new WP_Query( array( 'post_type' => 'projet', 'posts_per_page' => 6 ) ); ?>
+			<?php $loop = new WP_Query( array( 'post_type' => 'projet', 'posts_per_page' => 3 ) ); ?>
 			<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 				<?php get_template_part( 'template-parts/project-item' ); ?>
 			<?php endwhile; ?>
